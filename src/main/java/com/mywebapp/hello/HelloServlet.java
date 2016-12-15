15b5693fca84c1f823ac5d74d363e6195333b902
ctandy <tandy.charles@gmail.com>
2016-12-15 14:43:22 -0700

    Fixes

--- src/main/java/com/mywebapp/hello/HelloServlet.java
+++ src/main/java/com/mywebapp/hello/HelloServlet.java
@@ -8,6 +8,6 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
             throws IOException {
-        httpServletResponse.getWriter().print("Hello from HelloServlet");
+        httpServletResponse.getWriter().print("Hello from HelloServlet + 1");
     }
 }
